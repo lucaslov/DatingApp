7c8c2e909372db0a675d21a6fb8ad471d64fc165
Łukasz Czyż <czyzukasz2402@gmail.com>
2019-12-23 20:53:10 +0100

    Add client side login and register

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+import { AuthService } from './../_services/auth.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration success');
+    }, error => {
+      console.log(error);
+    });
+  }
+  cancel() {
+    console.log('canceled');
+    this.cancelRegister.emit(false);
+  }
+}
