196156a0fd4b8ba43a53fc5dac9e9cb4d7a8e45d
Łukasz Czyż <czyzukasz2402@gmail.com>
2019-12-31 00:45:45 +0100

    Configure error handling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -14,22 +14,22 @@ export class NavComponent implements OnInit {
   ngOnInit() {
   }
 
-  login(){
+  login() {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged in successfully');
     }, error => {
-      console.log('fail');
+      console.log(error);
     });
   }
 
-  loggedIn(){
+  loggedIn() {
     const token = localStorage.getItem('token');
     return !!token;
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('token');
-    console.log("logged out");
+    console.log('logged out');
   }
 
 }
